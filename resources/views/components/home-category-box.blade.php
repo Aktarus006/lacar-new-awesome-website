f4fab3dd7b7f82df562afec26c12910030692c79
Joachim Bastin <joachim.bastin@diagenode.com>
2021-09-06 11:56:21 +0200

    link for products box

--- resources/views/components/home-category-box.blade.php
+++ resources/views/components/home-category-box.blade.php
@@ -7,7 +7,7 @@
     </div>
     <div class="flex flex-col justify-between flex-1 p-6 bg-white">
         <div class="flex-1">
-            <a href="/software" class="block mt-2 cursor-pointer">
+            <a href="{{ $category['link'] }}" class="block mt-2 cursor-pointer">
                 <p class="text-xl font-semibold text-gray-900 hover:underline">
                     {{ $category['title'] }}
                 </p>

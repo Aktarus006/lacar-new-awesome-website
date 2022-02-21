5eca86e452cbe92645f0ccba9ad3969b669e74d4
Joachim Bastin <aktarus@Joachims-MacBook-Air.local>
2022-02-22 00:17:31 +0100

    more space

--- resources/views/categories/view.blade.php
+++ resources/views/categories/view.blade.php
@@ -37,28 +37,30 @@
                     <div class="px-3 py-0.5 mx-auto max-w-7xl sm:px-6 lg:px-8">
                     </div>
                 </div>
-                @foreach ($category->kits as $kit)
-                    <div class="w-screen max-w-lg space-y-2">
-                        <a href="/p/{{ $kit['slug'] }}">
-                            <div class="flex items-center rounded-full hover:text-white hover:bg-yellow-500">
-                                <button type="button"
-                                    class="inline-flex items-center p-1 text-white bg-yellow-500 border border-transparent rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-400">
-                                    <!-- Heroicon name: solid/plus -->
-                                    <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
-                                        fill="currentColor" aria-hidden="true">
-                                        <path fill-rule="evenodd"
-                                            d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
-                                            clip-rule="evenodd" />
-                                    </svg>
-                                </button>
-                                <p class="inline-flex items-center p-1 ml-3 text-lg font-medium ">
-                                    {{ $kit['name'] }}
-                                </p>
-                            </div>
-                        </a>
-                    </div>
+                <div class="space-y-4">
+                    @foreach ($category->kits as $kit)
+                        <div class="w-screen max-w-lg">
+                            <a href="/p/{{ $kit['slug'] }}">
+                                <div class="flex items-center rounded-full hover:text-white hover:bg-yellow-500">
+                                    <button type="button"
+                                        class="inline-flex items-center p-1 text-white bg-yellow-500 border border-transparent rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-400">
+                                        <!-- Heroicon name: solid/plus -->
+                                        <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+                                            fill="currentColor" aria-hidden="true">
+                                            <path fill-rule="evenodd"
+                                                d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
+                                                clip-rule="evenodd" />
+                                        </svg>
+                                    </button>
+                                    <p class="inline-flex items-center p-1 ml-3 text-lg font-medium ">
+                                        {{ $kit['name'] }}
+                                    </p>
+                                </div>
+                            </a>
+                        </div>
 
-                @endforeach
+                    @endforeach
+                </div>
             </div>
         </div>
     </div>

819e1e1be57d7b2027c3ab192ff5f16c109f5d2f
Joachim Bastin <aktarus@Joachims-MacBook-Air.local>
2022-02-21 01:29:55 +0100

    fixing piture

--- resources/views/components/team-member.blade.php
+++ resources/views/components/team-member.blade.php
@@ -2,7 +2,7 @@
 
 <div class="flex items-center space-x-4 lg:space-x-6">
     <img class="w-16 h-16 rounded-full lg:w-20 lg:h-20"
-        src="{{ $member->picture ?: 'https://i.pravatar.cc/256?u=' . $member->id }}" alt="">
+        src="{{ asset($member->picture) ?: 'https://i.pravatar.cc/256?u=' . $member->id }}" alt="">
     <div class="space-y-1 text-lg font-medium leading-6">
         <h3>{{ $member->name }}</h3>
         <p class="text-indigo-600">{{ $member->role }}</p>

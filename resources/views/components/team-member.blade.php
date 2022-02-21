dff7e675ea393c772f0e0c7e2498a1a0e5b5805b
Joachim Bastin <aktarus@Joachims-MacBook-Air.local>
2022-02-21 01:34:41 +0100

    fixing piture

--- resources/views/components/team-member.blade.php
+++ resources/views/components/team-member.blade.php
@@ -1,7 +1,7 @@
 @props(['member'])
 
 <div class="flex items-center space-x-4 lg:space-x-6">
-    <img class="w-16 h-16 rounded-full lg:w-20 lg:h-20"
+    <img class="object-contain w-16 h-16 rounded-full lg:w-20 lg:h-20"
         src="{{ asset(Voyager::image($member->picture)) ?: 'https://i.pravatar.cc/256?u=' . $member->id }}" alt="">
     <div class="space-y-1 text-lg font-medium leading-6">
         <h3>{{ $member->name }}</h3>

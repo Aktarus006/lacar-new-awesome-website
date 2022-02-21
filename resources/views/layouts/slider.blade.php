6720bc7d9b363f55adc4c7c9de217de6fdcb98ab
Joachim Bastin <aktarus@Joachims-MacBook-Air.local>
2022-02-21 02:00:37 +0100

    team icons

--- resources/views/layouts/slider.blade.php
+++ resources/views/layouts/slider.blade.php
@@ -1,18 +1,20 @@
-<div class="mb-8 bg-gray-800 h-96 ta-gallery ta-gallery-size" x-data="taGallery" x-init="init()" data-start="0"
-    data-timing="ease-in-out" data-duration="0.4s" data-autoplay="true" data-interval="8000" pauseonhover="true">
-    <!--- START SLIDES /-->
-    <div class="flex flex-col items-center justify-center px-12 py-8 overflow-hidden bg-center bg-cover bg-home-banner-1 ta-gallery-element ta-gallery-anim-scroll flex-center bg-opacity-95 bg-blur-1 sm:px-24 sm:py-12"
-        x-cloak>
-        <div class="text-base font-semibold leading-relaxed text-center text-gray-900 sm:text-xl">
-            <h2 class="text-3xl font-bold text-white sm:text-6xl text-shadow-lg">
-                Why are you still extracting ?
-            </h2>
-            <h3 class="m-5 text-lg font-semibold leading-normal text-white sm:text-2xl text-shadow-lg">
-                Your reference company for routine constitutional genetic testing
-            </h3>
+    <!--- DIV FOR SLIDER EFFECT /-->
+    {{-- <div class="mb-8 bg-gray-800 h-96 ta-gallery ta-gallery-size" x-data="taGallery" x-init="init()" data-start="0"
+    data-timing="ease-in-out" data-duration="0.4s" data-autoplay="true" data-interval="8000" pauseonhover="true"> --}}
+    <div class="mb-8 bg-gray-800 h-96 ta-gallery ta-gallery-size" x-data="taGallery" x-init="init()">
+        <!--- START SLIDES /-->
+        <div class="flex flex-col items-center justify-center px-12 py-8 overflow-hidden bg-center bg-cover bg-home-banner-1 ta-gallery-element ta-gallery-anim-scroll flex-center bg-opacity-95 bg-blur-1 sm:px-24 sm:py-12"
+            x-cloak>
+            <div class="text-base font-semibold leading-relaxed text-center text-gray-900 sm:text-xl">
+                <h2 class="text-3xl font-bold text-white sm:text-6xl text-shadow-lg">
+                    Why are you still extracting ?
+                </h2>
+                <h3 class="m-5 text-lg font-semibold leading-normal text-white sm:text-2xl text-shadow-lg">
+                    Your reference company for routine constitutional genetic testing
+                </h3>
+            </div>
         </div>
-    </div>
-    {{-- <div class="flex flex-col items-center justify-center px-12 py-8 overflow-hidden bg-cover bg-home-banner-2 ta-gallery-element ta-gallery-anim-scroll flex-center bg-opacity-95 bg-blur-1 sm:px-24 sm:py-12"
+        {{-- <div class="flex flex-col items-center justify-center px-12 py-8 overflow-hidden bg-cover bg-home-banner-2 ta-gallery-element ta-gallery-anim-scroll flex-center bg-opacity-95 bg-blur-1 sm:px-24 sm:py-12"
         x-cloak>
         <div class="text-base font-semibold leading-relaxed text-center text-gray-900 sm:text-xl">
             <h2 class="text-3xl font-bold text-white sm:text-4xl text-shadow-lg">
@@ -25,14 +27,14 @@
                 More Information</button>
         </div>
     </div> --}}
-    <!--- END SLIDES /-->
-    <!--- START BACKGROUND /-->
-    <div
-        class="flex flex-col items-center justify-center overflow-hidden text-center bg-black shadow-xl ta-gallery-background">
-    </div>
-    <!--- END BACKGROUND /-->
-    <!--- START BUTTONS /-->
-    <button type="button"
+        <!--- END SLIDES /-->
+        <!--- START BACKGROUND /-->
+        <div
+            class="flex flex-col items-center justify-center overflow-hidden text-center bg-black shadow-xl ta-gallery-background">
+        </div>
+        <!--- END BACKGROUND /-->
+        <!--- START BUTTONS /-->
+        {{-- <button type="button"
         class="flex items-center justify-center w-10 h-10 text-white bg-black bg-opacity-75 border-2 border-white rounded-full shadow-xl ta-gallery-button -left-5 sm:left-2 focus:ring focus:ring-primary"
         x-on:click="previous()" x-show="loaded">
         <svg class="w-6 h-6 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512">
@@ -49,6 +51,6 @@
                 d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z">
             </path>
         </svg>
-    </button>
-    <!--- END BUTTONS /-->
-</div>
+    </button> --}}
+        <!--- END BUTTONS /-->
+    </div>

84feb0e6f6bb6bc45657d500ab558d808cb2669b
Joachim Bastin <aktarus@Joachims-MacBook-Air.local>
2022-02-22 00:40:46 +0100

    job

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -25,7 +25,7 @@
         </div>
     </div>
 
-    @if (count($jobs) > 0)
+    @if (count($jobs) > 2)
         <section class="text-gray-600 body-font">
             <div class="container px-5 py-24 mx-auto">
                 <div class="flex flex-wrap -m-4">

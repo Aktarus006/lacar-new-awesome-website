0d7a187698ee5fff58c1802fc6e11cf565793ea3
Joachim Bastin <aktarus@Joachims-MacBook-Air.local>
2022-03-11 00:09:07 +0100

    feature: adding success message on top of the page when contact

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -10,6 +10,21 @@
     @include('layouts.navigation')
 
     <div class="mt-24">
+        @if ($success = session('success'))
+            <div class="w-full p-5 bg-green-100 rounded-md">
+                <div class="flex justify-between">
+                    <div class="flex space-x-3">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
+                            class="flex-none w-4 h-4 text-green-500 fill-current">
+                            <path
+                                d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-1.25 16.518l-4.5-4.319 1.396-1.435 3.078 2.937 6.105-6.218 1.421 1.409-7.5 7.626z" />
+                        </svg>
+                        <div class="flex-1 text-sm font-medium leading-tight text-green-700">{{ $success }}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endif
         @yield('content')
         @include('layouts.footer')
     </div>
